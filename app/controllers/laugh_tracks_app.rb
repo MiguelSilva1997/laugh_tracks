67887f114ea59c6ba5a512240df6daffc2cc8713
Miguel Silva <msilva0897@gmail.com>
2017-10-06 10:59:43 -0600

    Comedians challenge

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,2 +1,12 @@
 class LaughTracksApp < Sinatra::Base
+
+  get '/comedians' do
+    if params[:age]
+      @comedians = Comedian.where("age = ?", params[:age])
+    else
+      @comedians = Comedian.all
+    end
+    erb :index
+  end
+
 end
